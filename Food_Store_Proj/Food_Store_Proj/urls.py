4f63c6c11d26f944753361001e10a8a6d479055d
Shabin Bharath <shabinbharath555@gmail.com>
2025-01-07 18:48:27 +0530

    added user_app,need to create authentication part

--- Food_Store_Proj/Food_Store_Proj/urls.py
+++ Food_Store_Proj/Food_Store_Proj/urls.py
@@ -15,11 +15,52 @@ Including another URLconf
 """
 # Food_Store_Proj/urls.py
 
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('Chef_App.urls')),  # Include chef app URLs under /api/
+#     path('api/', include('Dish_App.urls')),  # Include dish app URLs under /api/
+# ]
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+# from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt import authentication
+from django.urls import path, include,re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   authentication_classes=(authentication.JWTAuthentication,),
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Chef_App.urls')),  # Include chef app URLs under /api/
-    path('api/', include('Dish_App.urls')),  # Include dish app URLs under /api/
+    path('api/chef', include('Chef_App.urls')),  
+    path('api/dish', include('Dish_App.urls')),   
+    path('api/user/', include('user_app.urls')),
+
+    path('api/token/',swagger_auto_schema(methods=['post'],security=[])(TokenObtainPairView.as_view())),
+    path('api/token/refresh/',swagger_auto_schema(methods=['post'],security=[])(TokenRefreshView.as_view())),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
